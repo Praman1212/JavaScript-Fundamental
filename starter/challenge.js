2346591d1675e2b34d5c5f89f206bc99d9bfd755
Praman <pramang017357@nec.edu.np>
2022-09-18 21:27:43 +0545

    Fourth Commit

--- starter/challenge.js
+++ starter/challenge.js
@@ -1,15 +1,33 @@
 //First challenge
 
-const ageMark = 78;
-const heightMark = 1.69;
-const ageJohn = 92;
-const heightJohn = 1.95;
+// const ageMark = 78;
+// const heightMark = 1.69;
+// const ageJohn = 92;
+// const heightJohn = 1.95;
 
-const markBMI = ageMark/(heightMark ** 2);
-console.log(markBMI); 
+// const markBMI = ageMark/(heightMark ** 2);
+// console.log(markBMI); 
 
-const johnBMI = ageJohn/(heightJohn ** 2);
-console.log(johnBMI);
+// const johnBMI = ageJohn/(heightJohn ** 2);
+// console.log(johnBMI);
 
-const markHigherBMI = markBMI > johnBMI;
-console.log(markBMI,'>',johnBMI,'=',markHigherBMI); 
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI,'>',johnBMI,'=',markHigherBMI); 
+const firstName = 'Jonas'
+const job = 'teacher'
+const birthYear = 1990;
+const year = 2022;
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job +' !';
+console.log(jonas)
+
+const jonasNew = `I'm ${firstName}, a ${(year - birthYear)} years old ${job} !`;
+console.log(jonasNew);
+
+// Multiple line string 
+console.log('Im \n\
+Praman \n\
+Ghimire');
+
+console.log(`I am 
+Praman
+Ghimire`);
