6d77a1da96ab098121fda485899c5eca22a4f03a
Praman <pramang017357@nec.edu.np>
2022-09-18 08:55:15 +0545

    second commit

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,37 @@
-let js = "amazing";
-      if (js == 'amazing') {alert('Javascript is FUN')};
+/* let js = "amazing";
+      // if (js == 'amazing') {alert('Javascript is FUN')};
       10+20+15;//can't visible in console 
       console.log(10+20+15);
+      let fstName = "Praman";
+      console.log(fstName);
+      // let year = 3;
+      let PI = 1.52;
+      let myFirstJob = 'Programmer';
+      let myCurrentJob = 'Teacher';
+
+      console.log(myFirstJob);
+      */
+//Variable name convension
+
+      // Datatype
+      //1. Number
+      //2.string
+      //3.Boolena
+      //4.Undefine: let child; it means child have empty value
+    /* let javaScript = true;
+      console.log(javaScript);
+      console.log(typeof javaScript);
+      console.log(typeof 23);
+      console.log(typeof 'Praman');
+      javaScript = "FUN";console.log(typeof javaScript)
+
+      //Undefine
+      let year;
+      year = 1991;
+      console.log(year);
+      console.log(typeof year);
+
+      console.log(typeof null);
+      */
+     let age = 30;
+     age = 31;
